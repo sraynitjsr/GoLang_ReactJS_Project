3c8018f2097d8a26b02cf37f71a471d566610bb1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-17 12:55:11 +0530

    Adding Home API

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+type ResponseData struct {
+	Message string `json:"message"`
+}
+
+func helloHandler(c *fiber.Ctx) error {
+	responseData := ResponseData{
+		Message: "Hello, World!",
+	}
+
+	return c.JSON(responseData)
+}
 
 func main() {
-  fmt.Println("REST API React Project")
+	app := fiber.New()
+
+	// Configure CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "https://gofiber.io, https://gofiber.net",
+		AllowMethods:     "GET",
+		AllowHeaders:     "Origin, Content-Type",
+		AllowCredentials: true,
+	}))
+
+	// Define the GET endpoint
+	app.Get("/", helloHandler)
+
+	// Start the Fiber app
+	err := app.Listen(":3000")
+	if err != nil {
+		panic(err)
+	}
 }
