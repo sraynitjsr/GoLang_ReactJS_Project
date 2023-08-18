7f6e42a0c83c47549142195fdbe71442ac58208f
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-18 16:51:30 +0530

    Adding home route

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,20 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber"
 )
 
-type ResponseData struct {
-	Message string `json:"message"`
+func helloWorld(c *fiber.Ctx) {
+	c.Send("Hello, World!")
 }
 
-func helloHandler(c *fiber.Ctx) error {
-	responseData := ResponseData{
-		Message: "Hello, World!",
-	}
-
-	return c.JSON(responseData)
+func setupRoutes(app *fiber.App) {
+	app.Get("/", helloWorld)
 }
 
 func main() {
 	app := fiber.New()
 
-	// Configure CORS
-	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://gofiber.io, https://gofiber.net",
-		AllowMethods:     "GET",
-		AllowHeaders:     "Origin, Content-Type",
-		AllowCredentials: true,
-	}))
-
-	// Define the GET endpoint
-	app.Get("/", helloHandler)
-
-	// Start the Fiber app
-	err := app.Listen(":3000")
-	if err != nil {
-		panic(err)
-	}
+	setupRoutes(app)
+	app.Listen(3000)
 }
