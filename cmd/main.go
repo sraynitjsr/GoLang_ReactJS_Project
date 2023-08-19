40306244b1ba569bf8ed35f1b52ef86139cd2650
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-20 01:13:23 +0530

    home (/) route with CORS policy

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-  "github.com/gofiber/fiber/v2"
-  "github.com/gofiber/fiber/v2/middleware/cors"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func helloWorld(c *fiber.Ctx) {
-	c.Send("Hello, World!")
-}
-
-func setupRoutes(app *fiber.App) {
-	app.Get("/", helloWorld)
+func home(c *fiber.Ctx) error {
+	return c.JSON("Hey World")
 }
 
 func main() {
 	app := fiber.New()
-	app.Use(cors.New())
-	
-	setupRoutes(app)
-	app.Listen(3000)
+
+	app.Use(cors.New(cors.Config{
+		Next:             nil,
+		AllowOriginsFunc: nil,
+		AllowOrigins:     "*",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodHead,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
+		AllowHeaders:     "",
+		AllowCredentials: false,
+		ExposeHeaders:    "",
+		MaxAge:           0,
+	}))
+
+	app.Get("/", home)
+	app.Listen(":3000")
 }
