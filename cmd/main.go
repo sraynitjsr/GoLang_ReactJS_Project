8445b843b0cf19ba1fa09d2d1a9ee91f441c1065
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-08-23 15:47:24 +0530

    Adding Main File

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sray/cmd/config"
+	"github.com/sray/cmd/handler"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", handler.HelloHandler)
+
+	app.Get("/getAllStudents", handler.GetAllStudents)
+
+	app.Listen(config.AppPort)
+}
